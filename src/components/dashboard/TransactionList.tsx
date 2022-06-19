b4d173994ca37f7987827c50d991b3f1a03f0614
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-19 14:33:28 -0300

    feat: add minus symbol before transaction value on transactions list

--- src/components/dashboard/TransactionList.tsx
+++ src/components/dashboard/TransactionList.tsx
@@ -16,11 +16,15 @@ export const TransactionList = ({
   const transactionTypeColor =
     transactionType === "income" ? "text-green-500" : "text-red-500";
 
+  const transactionValue = transactionType === "income" ? "" : "-";
+
   return (
     <div className="px-6 mb-4">
       <div className="flex flex-col bg-white p-4 rounded-lg">
         <span className="text-sm">{title}</span>
-        <span className={`text-xl ${transactionTypeColor}`}>{value}</span>
+        <span className={`text-xl ${transactionTypeColor}`}>
+          {transactionValue + value}
+        </span>
 
         <div className="flex justify-between text-sm text-text mt-5">
           <span>{category}</span>
