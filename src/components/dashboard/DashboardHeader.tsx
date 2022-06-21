43857e16b601c46c03dac76e554d89a180c13100
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-20 23:17:14 -0300

    style: fix dashboard header max width

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -8,7 +8,7 @@ const DashboardHeader = ({ openModal }: IProps) => {
   return (
     <div>
       <header className="bg-primary h-52 p-6">
-        <div className="flex justify-between max-w-4xl m-auto">
+        <div className="flex justify-between max-w-4xl px-6 m-auto">
           <div className="flex items-center">
             <div className="bg-secondary p-2 rounded-full mr-2">
               <FiDollarSign className="text-white text-xl" />
