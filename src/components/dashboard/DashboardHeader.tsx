1a0ae6cbf71fb4a277fb60c4a0cb1e88704d11e7
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-22 14:36:22 -0300

    style: improved dashboard header responsivity

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -7,17 +7,19 @@ interface IProps {
 const DashboardHeader = ({ openModal }: IProps) => {
   return (
     <div>
-      <header className="bg-primary h-52 p-6">
+      <header className="bg-primary h-52 py-6">
         <div className="flex justify-between max-w-4xl px-6 m-auto">
           <div className="flex items-center">
             <div className="bg-secondary p-2 rounded-full mr-2">
               <FiDollarSign className="text-white text-xl" />
             </div>
-            <span className="text-white font-semibold text-lg">Dev Money</span>
+            <span className="text-white font-semibold text-sm md:text-lg">
+              Dev Money
+            </span>
           </div>
           <button
             onClick={openModal}
-            className="bg-button hover:bg-buttonHover active:bg-buttonActive transition-all font-semibold text-white py-2 px-4 rounded"
+            className="bg-button hover:bg-buttonHover active:bg-buttonActive transition-all font-semibold text-white py-2 px-4 rounded text-sm md:text-lg ml-4 md:ml-0"
           >
             Nova transação
           </button>
