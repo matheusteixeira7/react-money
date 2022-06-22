7c5b3e9609674642756912f39f87b14efc2a317e
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-21 21:16:38 -0300

    feat: add validation and error handling to add transaction form

--- src/components/dashboard/AddTransactionModal.tsx
+++ src/components/dashboard/AddTransactionModal.tsx
@@ -1,17 +1,34 @@
 import axios from "axios";
-import { useFormik } from "formik";
+import { Field, Form, Formik } from "formik";
 import { useState } from "react";
 import { BsArrowDownCircle, BsArrowUpCircle } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
 import { v4 as uuidv4 } from "uuid";
+import * as Yup from "yup";
 
-interface IProps {
+interface IAddTransactionModalProps {
   handleModal: () => void;
   updateData: () => void;
 }
 
-export const AddTransactionModal = ({ handleModal, updateData }: IProps) => {
+export const AddTransactionModal = ({
+  handleModal,
+  updateData,
+}: IAddTransactionModalProps) => {
   const [transactionType, setTransactionType] = useState("");
+  const addTransactionSchema = Yup.object().shape({
+    title: Yup.string().required("Nome é obrigatório"),
+    value: Yup.number().required("Valor é obrigatório"),
+    type: Yup.string().required("Tipo de transação é obrigatório"),
+    category: Yup.string().required("Categoria é obrigatório"),
+  });
+
+  const initialValues = {
+    title: "",
+    value: "",
+    type: "",
+    category: "",
+  };
 
   const handleCloseModal = () => {
     handleModal();
@@ -20,37 +37,6 @@ export const AddTransactionModal = ({ handleModal, updateData }: IProps) => {
   const handleUpdateData = () => {
     updateData();
   };
-
-  const formik = useFormik({
-    initialValues: {
-      title: "",
-      value: "",
-      category: "",
-    },
-    onSubmit: (values) => {
-      const data = {
-        id: uuidv4(),
-        title: values.title,
-        value: values.value,
-        type: transactionType,
-        category: values.category,
-        date: new Date().toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "2-digit",
-          year: "2-digit",
-        }),
-      };
-
-      try {
-        axios.post("http://localhost:3333/transactions", data);
-
-        handleUpdateData();
-        handleCloseModal();
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  });
 
   return (
     <div className="">
@@ -64,61 +50,97 @@ export const AddTransactionModal = ({ handleModal, updateData }: IProps) => {
         />
       </div>
 
-      <form onSubmit={formik.handleSubmit}>
-        <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 mb-3 mt-6 leading-tight focus:outline-none focus:bg-white"
-          id="title"
-          name="title"
-          type="text"
-          placeholder="Nome"
-          onChange={formik.handleChange}
-          value={formik.values.title}
-        ></input>
-        <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="value"
-          name="value"
-          type="number"
-          placeholder="Valor"
-          onChange={formik.handleChange}
-          value={formik.values.value}
-        ></input>
-        <div className="grid grid-cols-2 gap-3 mb-3">
-          <div
-            className={`flex justify-center items-center hover:cursor-pointer border border-gray-300 py-3 px-4 rounded ${
-              transactionType === "income" ? "bg-green-100" : "bg-white"
-            }`}
-            onClick={() => setTransactionType("income")}
-          >
-            <BsArrowUpCircle className={`mr-2 text-green-500 text-2xl`} />
-            <span className="">Entrada</span>
-          </div>
-          <div
-            className={`flex justify-center items-center  hover:cursor-pointer border border-gray-300 py-3 px-4 rounded ${
-              transactionType === "outcome" ? "bg-red-100" : "bg-white"
-            }`}
-            onClick={() => setTransactionType("outcome")}
-          >
-            <BsArrowDownCircle className={`mr-2 text-red-500 text-2xl`} />
-            <span className="">Saída</span>
-          </div>
-        </div>
-        <input
-          className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-          id="category"
-          name="category"
-          type="text"
-          placeholder="Categoria"
-          onChange={formik.handleChange}
-          value={formik.values.category}
-        ></input>
-        <button
-          type="submit"
-          className="bg-secondary hover:bg-buttonHover active:bg-buttonActive transition-all font-semibold text-white py-2 px-4 rounded w-full"
-        >
-          Cadastrar
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={addTransactionSchema}
+        onSubmit={(values) => {
+          const data = {
+            id: uuidv4(),
+            title: values.title,
+            value: values.value,
+            type: values.type,
+            category: values.category,
+            date: new Date().toLocaleDateString("pt-BR", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "2-digit",
+            }),
+          };
+
+          try {
+            axios.post("http://localhost:3333/transactions", data);
+
+            handleUpdateData();
+            handleCloseModal();
+          } catch (error) {
+            console.log(error);
+          }
+        }}
+      >
+        {({ errors, touched, setFieldValue, values }) => (
+          <Form>
+            <Field
+              name="title"
+              type="text"
+              placeholder="Nome"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 mt-6 leading-tight focus:outline-none focus:bg-white"
+            />
+            {errors.title && touched.title ? (
+              <div className="text-red-500 mb-4 mt-1">{errors.title}</div>
+            ) : null}
+
+            <Field
+              name="value"
+              type="number"
+              placeholder="Valor"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 mt-3 leading-tight focus:outline-none focus:bg-white"
+            />
+            {errors.value && touched.value ? (
+              <div className="text-red-500 mb-4 mt-1">{errors.value}</div>
+            ) : null}
+
+            <div className="grid grid-cols-2 gap-3 mt-3">
+              <div
+                className={`flex justify-center items-center hover:cursor-pointer border border-gray-300 py-3 px-4 rounded ${
+                  values.type === "income" ? "bg-green-100" : "bg-white"
+                }`}
+                onClick={() => setFieldValue("type", "income")}
+              >
+                <BsArrowUpCircle className={`mr-2 text-green-500 text-2xl`} />
+                <span className="">Entrada</span>
+              </div>
+
+              <div
+                className={`flex justify-center items-center  hover:cursor-pointer border border-gray-300 py-3 px-4 rounded ${
+                  values.type === "outcome" ? "bg-red-100" : "bg-white"
+                }`}
+                onClick={() => setFieldValue("type", "outcome")}
+              >
+                <BsArrowDownCircle className={`mr-2 text-red-500 text-2xl`} />
+                <span className="">Saída</span>
+              </div>
+            </div>
+            {errors.type && touched.type ? (
+              <div className="text-red-500 mb-4 mt-1">{errors.type}</div>
+            ) : null}
+
+            <Field
+              name="category"
+              type="text"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-300 rounded py-3 px-4 mt-3 leading-tight focus:outline-none focus:bg-white"
+            />
+            {errors.category && touched.category ? (
+              <div className="text-red-500 mb-4 mt-1">{errors.category}</div>
+            ) : null}
+            <button
+              type="submit"
+              className="bg-secondary hover:bg-buttonHover active:bg-buttonActive transition-all font-semibold text-white py-2 px-4 mt-3 rounded w-full"
+            >
+              Cadastrar
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
