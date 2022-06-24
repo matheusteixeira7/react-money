1b83f861bfb9d25b7d096ab3ed9665e96576daf8
Matheus Teixeira <matheusteixeira@devnine.tech>
2022-06-23 22:30:18 -0300

    feat: add prisma adapter config and connect with postgres db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,14 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
 export default NextAuth({
-  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
